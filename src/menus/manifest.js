3c9d3c0241edd5396496a467cbe2ab688baa00f3
CGBSFERNANDES <c.gbs.fernandes@gmail.com>
2026-01-01 14:22:37 -0300

    Add Diario Contabil menu wrapper

--- src/menus/manifest.js
+++ src/menus/manifest.js
@@ -2,4 +2,5 @@ export default [
   { id: 8827, name: "Exemplo", pageSize: 200 },
   { id: 9999, name: "Meu menu 9999", pageSize: 200 },
   { id: 8833, name: "Lote Contábeis", pageSize: 200 },
+  { id: 8837, name: "Diário Contábil", pageSize: 200 },
 ];
