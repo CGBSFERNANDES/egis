a7bf797742b372748a2428de6f4cd67a7b264a16
CGBSFERNANDES <c.gbs.fernandes@gmail.com>
2026-01-03 15:25:07 -0300

    Add monitor de notas fiscais wrapper

--- src/menus/manifest.js
+++ src/menus/manifest.js
@@ -3,4 +3,5 @@ export default [
   { id: 9999, name: "Meu menu 9999", pageSize: 200 },
   { id: 8833, name: "Lote Contábeis", pageSize: 200 },
   { id: 8837, name: "Diário Contábil", pageSize: 200 },
+  { id: 8842, name: "Monitor de Notas Fiscais", pageSize: 200 },
 ];
