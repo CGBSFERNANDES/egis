2237c1963bcff21a1f005640667ac6c8e6017bc7
CGBSFERNANDES <c.gbs.fernandes@gmail.com>
2026-01-05 13:04:06 -0300

    Add relatório 417 etiqueta do remetente

--- SQL/pr_egis_relatorio_padrao.sql
+++ SQL/pr_egis_relatorio_padrao.sql
@@ -173,6 +173,13 @@ begin
 	return
 end
 
+--Etiqueta do Remetente------------------------------------------------------------------------------------------
+if @cd_relatorio = 417
+begin
+    exec pr_egis_relatorio_etiqueta_remetente @json
+    return
+end
+
 --Balanço Patrimonial------------------------------------------------------------------------------------------
 if @cd_relatorio = 412
 begin
